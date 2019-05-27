bcaa79d70538a2fbb7ead419c6b4b03cbe213665
Victor Monte <victor.monte@wirecard.com>
2019-05-27 20:37:24 -0300

    Add new scenas

---
+++ src/components/SobreJogo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {
+    Text
+} from 'react-native';
+
+export default class SobreJogo extends Component {
+    render() {
+        return (<Text>Teste</Text>);
+    }
+}
